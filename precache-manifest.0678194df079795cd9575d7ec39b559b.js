eb5a5b0c2c338ff14c67850c2cf6d009a3f1c68d
Keeveee <shypanties22@gmail.com>
2023-01-19 23:48:40 +0800

    first commit

---
+++ precache-manifest.0678194df079795cd9575d7ec39b559b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b6ca2048fd75f0dfa9f366b1cc8536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45b7bbde35bd8835624e",
+    "url": "/static/css/main.c74311d7.chunk.css"
+  },
+  {
+    "revision": "a9fe51c303c7c398e3d6",
+    "url": "/static/js/2.d2faa417.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.d2faa417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b7bbde35bd8835624e",
+    "url": "/static/js/main.a663b386.chunk.js"
+  },
+  {
+    "revision": "81e92740592d5a2692a8",
+    "url": "/static/js/runtime-main.6ae9b8e7.js"
+  },
+  {
+    "revision": "755bfc1a21aad070a8d1a4245977a498",
+    "url": "/static/media/bet.755bfc1a.jpeg"
+  },
+  {
+    "revision": "6dbb4c58942c1e364cb90a286adbb924",
+    "url": "/static/media/main-icon.6dbb4c58.svg"
+  },
+  {
+    "revision": "16394af4583ff629dd19f3255d303222",
+    "url": "/static/media/main-logo.16394af4.svg"
+  }
+]);
